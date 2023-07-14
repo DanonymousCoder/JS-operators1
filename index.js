c849ad449331f31976006054efaef233cb2f6926
Ridwan <aladebaruwa@gmail.com>
2023-07-14 21:59:52 +0100

    JS

--- index.js
+++ index.js
@@ -68,3 +68,18 @@ console.log(test);
 
 	// Less than or equal Operator
 	console.log(val1 <= val2);
+
+	<script>
+	// Assigning values
+	let val1 = 5;
+	let val2 = 5;
+	
+	// Logical !(NOT) Operator
+	console.log(!val2);
+
+	// Logical &&(AND) Operator
+	console.log( val1 && val2 );
+	
+	// Lgical ||(OR) Operator
+	console.log( val1 || val2 );
+</script>
